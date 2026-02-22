40647c6a6b28d0b134c79512164156971eed7689
Nujaba Irfan <nujaba30@gmail.com>
2026-02-22 17:14:21 +0530

    Added web server example

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 console.log("Hello NodeJS Lab 03");
+const http = require('http');
 
-const fs = require('fs');
-
-fs.writeFile('file.txt', 'Hello World!', function (err) {
-  if (err) throw err;
-  console.log('File saved!');
-});
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
